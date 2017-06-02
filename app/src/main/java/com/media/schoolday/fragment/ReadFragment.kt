481bb128c7ff616f31b9487bbb1653a8757e9429
yosi <alqhemy@gmail.com>
2017-06-03 02:50:36 +0700

    update all

--- app/src/main/java/com/media/schoolday/fragment/ReadFragment.kt
+++ app/src/main/java/com/media/schoolday/fragment/ReadFragment.kt
@@ -1,22 +1,43 @@
 package com.media.schoolday.fragment
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.CardView
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.ceylonlabs.imageviewpopup.ImagePopup
 import com.media.schoolday.R
-import com.media.schoolday.models.model.Comment
+import com.media.schoolday.SchoolApp
+import com.media.schoolday.models.model.*
+import com.media.schoolday.utility.BitmapScaler
 import com.media.schoolday.utility.DbLocal
-import com.media.schoolday.utility.loadImg
+import com.media.schoolday.utility.loadWithGlade
+import com.orhanobut.wasp.Callback
+import com.orhanobut.wasp.Response
+import com.orhanobut.wasp.WaspError
+import kotlinx.android.synthetic.main.activity_news.*
 import kotlinx.android.synthetic.main.fragment_find.view.*
 import kotlinx.android.synthetic.main.imageview_item.view.*
 import kotlinx.android.synthetic.main.list_item_find.view.*
 import kotlinx.android.synthetic.main.list_news_home.view.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.padding
+import org.jetbrains.anko.textColor
+import org.jetbrains.anko.toast
+import org.json.JSONObject
 import java.text.SimpleDateFormat
-import java.util.*
 
 /*
  * Created by yosi on 10/05/2017.
@@ -25,13 +46,17 @@ import java.util.*
 class ReadFragment: Fragment(),AnkoLogger{
 
     val KEY_ID = "id"
-    var find: String? = null
+    var find = ""
+    lateinit var imgPop: ImagePopup
     lateinit var adapter:ArrayAdapter<Comment>
-    interface FindListener {
-        fun AlertMesage(s: String)
-        fun onProcess(data: HashMap<String,String>) {}
+    lateinit var glidApp: Glide
+    lateinit var root: View
+    interface ReadNewsListener {
+        fun onMessage(message: String) {}
     }
-    var callback: FindListener? = null
+    var callback: ReadNewsListener? = null
+
+
 
     companion object {
         fun newInstance(s: String, id: String): ReadFragment {
@@ -53,45 +78,189 @@ class ReadFragment: Fragment(),AnkoLogger{
         if(arguments != null && arguments.containsKey(KEY_ID)){
             find = arguments.getString(KEY_ID)
         }
-        callback = activity as? FindListener
+        callback = activity as? ReadNewsListener
+        imagePopInitial()
+        glidApp = Glide.get(context)
+    }
+
+    fun imagePopInitial(){
+
+        val width = resources.displayMetrics.widthPixels
+        val heigh = resources.displayMetrics.heightPixels
+        imgPop = ImagePopup(context)
+        imgPop.apply {
+            setBackgroundColor(Color.BLACK)
+            setWindowWidth(width)
+            setWindowHeight(heigh)
+            setHideCloseIcon(true)
+            setImageOnClickClose(true)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater!!.inflate(R.layout.fragment_find,container,false)
+        root = inflater!!.inflate(R.layout.fragment_find,container,false)
         val news = DbLocal.newsList().filter { it.id == find }.first()
-        with(view){
+        if(DbLocal.getPostId()?.news?.id  != null && DbLocal.getPostId()?.news?.id == find)
+            createData(DbLocal.getPostId()!!)
+        else
+            getNewsId(find!!)
+
+        with(root){
+            tvHomeItemSekolah.text = news.sekolah
             tvHomeItemUser.text = news.userName
             tvHomeItemDate.text = SimpleDateFormat("dd-MM-yyyy HH:mm").format(news.timeCreated).toString()
             tvHomeItemTitle.text = news.title
             tvHomeItemDescription.text = news.description
-            tvHomeItemTopic.text = news.topic
             tvHomeItemUserTitle.text = news.userTitle
-        }
 
-        if (news.comments != null) {
-//            activity.longToast("commentar ada")
-            news.comments.forEach {
-                val view2 = LayoutInflater.from(activity.baseContext).inflate(R.layout.list_item_find, null)
-                view2.tvFindItemUser.text = it.user
-                view2.tvFindItemDescription.text = it.comment
-                view2.tvFindItemDate.text = SimpleDateFormat("dd-MM-yyyy HH:mm").format(it.timeCreated).toString()
-                view2.tvFindItemUserTitle.text = it.title
-                view.linearReadComment.addView(view2)
+            if(news.topic == "Umum") {
+                tvHomeItemTopic.text = news.category + " " + news.topic
+                activity.toolbarNews.title = news.category + " " + news.topic
+            }
+            else {
+                tvHomeItemTopic.text = news.category + " Kelas " + news.topic
+                activity.toolbarNews.title = news.category + " Kelas " + news.topic
             }
         }
+        if (news.comments != null)
+            createComent(news.comments)
+
+        if (news.photo != null)
+            createPhoto(news.photo)
+        activity.toolbarNews.setNavigationOnClickListener() {
+            activity.onBackPressed()
+        }
+
+        return root
+
+    }
+    fun createComent(comment: ArrayList<Comment>){
+
+        val komentar = TextView(context)
+        komentar.setText("Komentar...")
+//        val card = CardView(context)
+//        card.addView(komentar)
+        komentar.padding = 10
+        root.linearReadComment.addView(komentar)
+
+        comment.forEach {
+            val view2 = LayoutInflater.from(activity.baseContext).inflate(R.layout.list_item_find, null)
+            view2.tvFindItemUser.text = it.user
+            view2.tvFindItemDescription.text = it.comment
+            view2.tvFindItemDate.text = SimpleDateFormat("dd-MM-yyyy HH:mm").format(it.timeCreated).toString()
+            view2.tvFindItemUserTitle.text = it.title
+            root.linearReadComment?.addView(view2)
+        }
+    }
+    fun createPhoto(photo: ArrayList<Photo>){
         val url = "http://128.199.199.232/web/api/news/photo/"
-        if (news.photo != null) {
-            news.photo.forEach {
-                val viewPhoto = LayoutInflater.from(activity.baseContext).inflate(R.layout.imageview_item, null)
-
-                viewPhoto.image.setOnClickListener{
-                }
-                viewPhoto.image.loadImg(url + it.filename)
-                view.linearReadPicture.addView(viewPhoto)
+        photo.forEach {
+            val viewPhoto = LayoutInflater.from(context).inflate(R.layout.imageview_item, null)
+            viewPhoto.image.loadWithGlade(url + it.filename )
+            viewPhoto.image.isDrawingCacheEnabled = true
+            viewPhoto.image.setOnClickListener{
+                val bit = BitmapScaler.scaleToFitWidth(viewPhoto.image.drawingCache,activity.resources.displayMetrics.widthPixels)
+                val draw: Drawable = BitmapDrawable(context.resources,bit)
+                imgPop.initiatePopup( draw )
             }
+            root.linearReadPicture.addView(viewPhoto)
         }
-        return view
+    }
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        view?.ibNewsComment?.setOnClickListener {
+            sendComment(view.editNewsComment.text.toString())
+            view.editNewsComment.setText("")
 
+        }
+    }
+
+    fun sendComment(msg: String){
+        val comment = HashMap<String,String>()
+        comment.put("comment",msg)
+        SchoolApp.rest?.putNewsComment(find,DbLocal.token(),comment,object: Callback<NewsModel>{
+            override fun onSuccess(response: Response?, t: NewsModel?) {
+                root.linearReadComment?.removeAllViews()
+                createComent(t!!.comments)
+                callback?.onMessage("update news")
+                root.scrolNewsMain.post {
+                    run {
+                        root.scrolNewsMain.fullScroll(ScrollView.FOCUS_DOWN)
+                    }
+                }
+            }
+
+            override fun onError(error: WaspError?) {
+                context.toast("Network failed, please try again later..")
+            }
+        })
+    }
+    fun createData(t: ResponNewsId){
+
+        if(t?.aktifitas != null){
+            if(t.news.userId == DbLocal.getProfile()?.user?.email){
+                addAktifitas(t.aktifitas)
+            }
+            else {
+                val filter = DbLocal.getProfile()?.child?.map { it.nis }  as ArrayList<String>
+                addAktifitas( t.aktifitas.filter { it.nis in filter } as ArrayList<GetActivities> )
+            }
+
+        }
+    }
+
+    fun addAktifitas(list: ArrayList<GetActivities>){
+        root.linearReadAktifitas.removeAllViews()
+//        val siswa = DbLocal.getProfile()?.child?.map { it.nama } as ArrayList<String>
+        list.forEachIndexed { index, getActivities ->
+            var tvAnak = TextView(context)
+            var card = CardView(context)
+
+            card.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,96)
+            with(tvAnak){
+                text = list[index].name
+                padding = 20
+                gravity = Gravity.CENTER_VERTICAL
+                layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT)
+                textColor = resources.getColor(R.color.colorPrimaryDark)
+                setTypeface(Typeface.DEFAULT_BOLD)
+                setTextSize(TypedValue.COMPLEX_UNIT_SP, 14F)
+
+            }
+            tvAnak.setOnClickListener {
+                val id = list[index].nis
+                val ft = activity.supportFragmentManager.beginTransaction()
+                with(ft) {
+                    setCustomAnimations(
+                            R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+                    replace(com.media.schoolday.R.id.news_fragment, ReadDetailFragment.newInstance(find!!,id!!))
+                    addToBackStack(null)
+                    commit()
+                }
+            }
+            card.addView(tvAnak)
+            root.linearReadAktifitas.addView(card)
+        }
+
+    }
+
+    fun getNewsId(id: String){
+        SchoolApp.rest?.getNewsId(id, object : Callback<ResponNewsId>{
+            override fun onSuccess(response: Response?, t: ResponNewsId?) {
+                val userRef = JSONObject(response!!.body)
+                SchoolApp.prefs!!.saveJSONObject("news", "newsId", userRef)
+                if(t != null)
+                    createData(t)
+            }
+
+            override fun onError(error: WaspError?) {
+            }
+        })
+    }
+
+    override fun onDetach() {
+        callback = null
+        super.onDetach()
     }
 
 
